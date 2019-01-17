2c5301e9abc1586684afb31e2e13b8f9b3d378bc
Shahynkamali <shahynkamali@gmail.com>
2019-01-17 17:59:25 -0500

    created functionallity

---
+++ src/components/Weather.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Weather = props =>(
+		<div>
+			{props.city && props.country && <p>Location: {props.city} {props.country}</p>}
+			{props.temprature && <p>Temprature: {props.temprature}</p>}
+			{props.humidity && <p>Humidity: {props.humidity}</p> }
+			{props.description && <p>Conditions: {props.description}</p> }
+			{props.error && <p>{props.error}</p>}
+		</div>
+	
+
+
+
+	);
+
+
+export default Weather;
